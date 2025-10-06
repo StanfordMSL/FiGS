4fe8d88bf5cccea24a6c75bd12d2e01a8143b64e
JunEn Low <lowjunen@gmail.com>
2025-10-06 16:17:44 -0700

    Best mpc?

---
+++ src/figs/control/state_machines.py
@@ -0,0 +1,42 @@
+import time
+import shutil
+import os
+import numpy as np
+from typing import Literal
+from enum import Enum
+
+import figs.utilities.config_helper as ch
+import figs.utilities.transform_helper as th
+import figs.dynamics.quadcopter_rate_model as qrm
+
+from casadi import Function
+from acados_template import AcadosOcp, AcadosOcpSolver
+from figs.control.base_controller import BaseController
+from figs.dynamics.external_forces import ExternalForces
+from figs.visualize import rich_visuals as rv
+
+class bMode(Enum):
+    RESET = 0
+    APPROACH = 1
+    FINETUNE = 2
+    CONTACT = 3
+
+def check_conditions( conditions:dict[str, float|None], tolerances:dict[str,float|None]):
+    """
+    Method to check if the current conditions meet the specified tolerances.
+
+    Args:
+        - conditions: Current conditions dictionary.
+        - tolerances: Tolerances dictionary.
+    """
+
+    for key,val in tolerances.items():
+        if val is None:
+            continue  # automatically true
+        else:
+            if isinstance(val, list):
+                if val[0] == "leq" and conditions[key] > val[1]:
+                    return False
+                elif val[0] == "geq" and conditions[key] < val[1]:
+                    return False
+    return True
--- src/figs/utilities/config_helper.py
+++ src/figs/utilities/config_helper.py
@@ -5,7 +5,7 @@ import json
 
 from pathlib import Path
 
-def get_config(config_name:str, config_type:str,configs_path:Path=None) -> dict: 
+def get_config(config_name:str, config_type:str,configs_path:Path=None) -> dict[str,dict[str,dict|int|float|list[float]]]: 
     """"
     Load a configuration file from the corresponding configs directory.
 
